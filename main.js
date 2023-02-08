dc67bcbe95c592bb6ef124ebaef244fd700a1656
NJL <naeljl@protonmail.com>
2023-02-08 09:51:56 +0100

    date

--- main.js
+++ main.js
@@ -3,35 +3,27 @@ if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(location => {
         const long = location.coords.longitude;
         const lat = location.coords.latitude;
+        getWeatherData(long, lat)
     }, () => {
         alert("Vous avez refusé la géolocalisation, l'application ne peut fonctionner sans, veuillez l'activer.")
     })
 }
 
-// HEADER --------------------------------------------------------------------
 
-// CHANGER LA TEMPERATURE ALEATOIREMENT
-// tableau de températures
+
+// Changer la température aléatoirement
 let tempArray = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 60, 100];
-// aller chercher la variable de la température à modifier
 const temp = document.querySelector('.main-temperature');
-// créer la variable qui contiendra l'index aléatoire
 let varRandomTemp = 0;
-// créer la fonction qui appelle un index alétoire et l'affiche
 function randomTemp () {
-    // créer la variable qui choisira un index aléatoire
     let random = Math.floor(Math.random() * tempArray.length);
-    // modifier la variable
     varRandomTemp = tempArray[random];
-    // l'afficher dans la case
     temp.innerHTML = `${varRandomTemp}°C`;
 };
-// appeler la fonction
 randomTemp();
 
 
-// CHANGER L'IMAGE EN FONCTION DE LA TEMPERATURE
-// tableau d'images
+// Changer l'image en fonction de la température
 let imgArray = [
     'images/cloud.png',
     'images/rain.png',
@@ -39,9 +31,7 @@ let imgArray = [
     'images/night.png',
     'images/rocket.png'
 ];
-// aller chercher la variable de l'image à modifier
 const image = document.querySelector('.main-weather-image');
-// créer la fonction qui affiche l'image sous conditions
 function showImage () {
     if (varRandomTemp <= 10) {
         image.src = imgArray[1];
@@ -55,50 +45,56 @@ function showImage () {
         image.src = imgArray[3];
     }
 };
-// appeler la fonction
 showImage();
 
 
-// CHANGER LE JOUR AUTOMATIQUEMENT
-// quel jour sommes-nous
-const currentDay = new Date().toLocaleDateString("fr-FR", {weekday: "long"})
-// aller chercher la variable du jour à modifier
-const currentDate = document.querySelector(".current-date");
-// créer la fonction qui modifie la valeur du jour en cours
+// Changer le jour actuel
+let currentDate = document.querySelector(".current-date");
+const currentDay = new Date().toLocaleDateString("fr-FR", {weekday: "long", day: "numeric", month: "long"});
 function currentDayDate (e) {
     currentDate.innerHTML = e.toUpperCase();
 }
-// appeler cette fonction
 currentDayDate(currentDay);
 
 
-// NAV ------------------------------------------------------------------------
+// Changer les autres jours
+let plusOne = document.querySelector('.plusOne');
+let plusTwo = document.querySelector('.plusTwo');
+let plusThree = document.querySelector('.plusThree');
+let plusFour = document.querySelector('.plusFour');
+let plusFive = document.querySelector('.plusFive');
+let plusSix = document.querySelector('.plusSix');
+let plusSeven = document.querySelector('.plusSeven');
+let tomorrow = new Date();
 
-// CHANGER LA TEMPERATURE ALEATOIREMENT POUR CHAQUE CLASS
-// tableau de température récupéré
-// aller chercher les variables de température à modifier => renvoit un tableau
+function plusDate (plus) {
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrowString = tomorrow.toLocaleDateString("fr-FR", {weekday: "long", day: "numeric"});
+    plus.innerHTML = tomorrowString.toLowerCase();
+}
+
+plusDate(plusOne);
+plusDate(plusTwo);
+plusDate(plusThree);
+plusDate(plusFour);
+plusDate(plusFive);
+plusDate(plusSix);
+plusDate(plusSeven);
+
+
+// Changer la température aléatoirement pour chaque class
 const tempNav = document.querySelectorAll('.temperature');
-// créer la variable qui contiendra l'index aléatoire
 let varRandomTempNav = 0;
-// parcourir le tableau de class pour appliquer la variable modifiée
 tempNav.forEach((element) => {
-    // créer un nombre aléatoire
     let randomNav = Math.floor(Math.random() * tempArray.length);
-    // modifier la variable en lui appliquant l'index issu du nombre aléatoire
     varRandomTempNav = tempArray[randomNav];
-    // modifier le DOM en affichant la variable
     element.innerHTML = `${varRandomTempNav}°C`;
 });
 
 
-// MODIFER L'IMAGE ALEATOIREMENT
-// tableau des images récupéré
-// aller chercher les variables de l'image à modifier
+// Changer l'image aléatoirement pour chaque class
 const imageNav = document.querySelectorAll('.weather-image');
-// pour chaque élément du tableau de class
 imageNav.forEach ((element) => {
-    // on attribue un nombre aléatoire en fonction du tableau d'images
     let randomImageNav = Math.floor(Math.random() * imgArray.length);
-    // on change l'image aléatoirement
     element.src = imgArray[randomImageNav];
 })
